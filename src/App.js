a6d87386fa5491569b470707cc034341411a53de
chinmaykhandal <khandalchinmay@gmail.com>
2022-06-20 15:11:23 +0530

    New UI

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import WeatherApp from './components/WeatherApp';
+function App() {
+  return (
+    <div>
+    <div className="App">
+      <WeatherApp />
+    </div>
+    </div>
+  );
+}
+
+export default App;
