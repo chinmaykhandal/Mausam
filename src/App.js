cc16f2d52fc9e3f68616752595a8dfa1b9dafbf3
Chinmay <khandalchinmay@gmail.com>
2022-06-26 17:41:27 +0530

    Added Forecasting feature :heartpulse:

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import WeatherApp from './components/WeatherApp';
+import Container from 'react-bootstrap/Container'
 function App() {
   return (
     <div>
+    <Container> 
     <div className="App">
       <WeatherApp />
     </div>
+    </Container>
     </div>
   );
 }
--- src/components/WeatherApp.js
+++ src/components/WeatherApp.js
@@ -1,34 +1,102 @@
 import React, { useState } from "react";
 import "./WeatherApp.css";
-import Stack from "@mui/material/Stack";
-import Button from "@mui/material/Button";
 import {TextField} from '@mui/material';
+import Box from '@mui/material/Box';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+
 
 const WeatherApp = () => {
-  let obj = { temp: 0, min: 0, max: 0 };
+  let obj = { temp: 0, feelsLike: 0, lon: 0, lat: 0 };
   const [city, setCity] = useState(obj);
   const [search, setSearch] = useState("Please enter the city name");
-
+  const [isSelected, setIsSelected] = useState(false);
+  const [forecast, setForecast] = useState([]);
   const fetchApi = async (city_name) => {
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city_name}&appid=a6454549915398c25d1e63ce329debc0`;
     const response = await fetch(url);
     const resJson = await response.json();
     //let main = JSON.parse(resJson);
     console.log(resJson["main"]);
-    let temp_num = (resJson["main"]["temp"] - 273.15).toFixed(2);
-    let temp_min_num = (resJson["main"]["temp_min"] - 273.15).toFixed(2);
-    let temp_max_num = (resJson["main"]["temp_max"] - 273.15).toFixed(2);
+    let temp_num = (resJson["main"]["temp"] - 273.15).toFixed(0);
+    let feels_like = (resJson["main"]["feels_like"] - 273.15).toFixed(0);
+    let longitude = resJson["coord"]["lon"];
+    let lattitude = resJson["coord"]["lat"];
     setCity({
       temp: temp_num,
-      min: temp_min_num,
-      max: temp_max_num,
+      feelsLike: feels_like,
+      lon: longitude,
+      lat: lattitude,
       weather: resJson["weather"]["0"]["main"],
     });
   };
 
+  const getForecast = async () => {
+          const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${city.lat}&lon=${city.lon}&appid=a6454549915398c25d1e63ce329debc0`;
+          const response = await fetch(url);
+          const resJson = await response.json();
+          let obj = [];
+          let list = resJson["list"];
+          let a = 0;
+          for(let i=0;i < 40;i++)
+          {
+              if(i%8 === 0)
+              {
+                var myDate = new Date(list[i]["dt"]*1000);
+
+                obj.push({key: a, dt: myDate, 
+                temp: (list[i]["main"]["temp"]-273.15).toFixed(0), 
+                feelsLike: (list[i]["main"]["feels_like"]-273.15).toFixed(0)})
+                a = a+1;
+              }   
+          }
+          setForecast(obj);
+          console.log(forecast);
+          
+  };
+
+  const monthNames = ["January", "February", "March", "April", "May", "June",
+  "July", "August", "September", "October", "November", "December"
+];
+
+  const card = (
+  forecast.map(artist => (
+    <div key={artist.key} style={{border: '1px solid black'}}>
+        <React.Fragment>
+    <CardContent>
+      <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+        {(artist.dt).getDate()}, {monthNames[(artist.dt.getMonth())]}
+      </Typography>
+      
+      <Typography sx={{ mb: 1.5 }} color="text.secondary">
+       Temperature : {artist.temp}
+      </Typography><Typography variant="body2">
+        well meaning and kindly.
+        <br />
+        {'"a benevolent smile"'}
+      </Typography>
+      
+    </CardContent>
+    
+  </React.Fragment>
+    </div>
+  )))
+
+  const renderForecast = () => {
+    return (
+      <Box sx={{ minWidth: 275 }}>
+        <Card variant="outlined">{card}</Card>
+      </Box>
+    );
+  }
+
   const getWeather = () => {
     console.log("hiiiiiii", search);
     fetchApi(search);
+    setIsSelected(true);
   };
 
   return (
@@ -51,16 +119,13 @@ const WeatherApp = () => {
           
         </div>
 
-        <div className="info">
-          <h2 className="location">
-            <p>{search}</p>
-          </h2>
-          <h1 className="temp">{city.temp}°Cel</h1>
-          <h3 className="tempMinMax">
-            Min : {city.min}°Cel | Max : {city.max}°Cel
-          </h3>
+        {isSelected && <div className="info">
+          <h1 className="temp">{city.temp}°C</h1>
+          <h2>Feels Like : {city.feelsLike}°C</h2>
           <h3>{city.weather}</h3>
-        </div>
+          <Button variant="contained" onClick={getForecast} style={{marginTop:10}}>Get Forecast</Button><br /><br />
+          {renderForecast()}
+        </div>  }
         
       </div>
     </>
